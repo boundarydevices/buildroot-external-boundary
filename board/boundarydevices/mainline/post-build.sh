d5e24b9f138acf9930f8e59a743f8d1fa0ec4069
Gary Bisson <gary.bisson@boundarydevices.com>
2018-01-08 12:20:51 +0100

    configs/nitrogen6x_qt5_gst1_mainline_defconfig: bump kernel to 4.14
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- board/boundarydevices/mainline/post-build.sh
+++ board/boundarydevices/mainline/post-build.sh
@@ -6,14 +6,21 @@
 #
 
 BOARD_DIR="$(dirname $0)"
+BOOTSCRIPTS_DIR="$UBOOT_DIR/board/boundary/bootscripts"
 
-# bd u-boot looks for bootscript here
+# bd u-boot looks for standard bootscript
+install -m 0644 -D $BINARIES_DIR/boot.scr $TARGET_DIR/boot/
+# legacy 6x_bootscript script
 $HOST_DIR/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
 -n "boot script" -d $BOARD_DIR/6x_bootscript.txt $TARGET_DIR/6x_bootscript
 
 # u-boot / update script for bd upgradeu command
 if [ -e $BINARIES_DIR/u-boot.imx ];
 then
+    install -D -m 0644 $BINARIES_DIR/u-boot.imx $TARGET_DIR/u-boot.imx
+    $HOST_DIR/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
+    -n "upgrade script" -d $BOOTSCRIPTS_DIR/upgrade.txt $TARGET_DIR/upgrade.scr
+    # legacy 6x_upgrade script
     install -D -m 0644 $BINARIES_DIR/u-boot.imx $TARGET_DIR/u-boot.imx
     $HOST_DIR/bin/mkimage -A arm -O linux -T script -C none -a 0 -e 0 \
     -n "upgrade script" -d $BOARD_DIR/6x_upgrade.txt $TARGET_DIR/6x_upgrade
