3adae5c61ae98015be800744db4ed426db39e90b
Gary Bisson <gary.bisson@boundarydevices.com>
2020-12-30 11:00:07 +0100

    package/gst1-imx-v2: bump rev to 11827510
    - add g2d support
    - various fixes
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- package/gst1-imx-v2/gst1-imx-v2.mk
+++ package/gst1-imx-v2/gst1-imx-v2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GST1_IMX_V2_VERSION = 038ec0b65270d0412a6be2cf1d2f63a9aaac0e80
+GST1_IMX_V2_VERSION = 1182751053ca5bc246ab5d1560ae8667ab15640f
 GST1_IMX_V2_SITE = $(call github,Freescale,gstreamer-imx,$(GST1_IMX_V2_VERSION))
 GST1_IMX_V2_LICENSE = LGPL-2.0+
 GST1_IMX_V2_LICENSE_FILES = LICENSE
@@ -16,5 +16,9 @@ GST1_IMX_V2_DEPENDENCIES += \
 	gst1-plugins-base \
 	imx-codec \
 	libimxvpuapi-v2
+
+ifeq ($(BR2_PACKAGE_IMX_GPU_G2D),y)
+GST1_IMX_V2_DEPENDENCIES += imx-gpu-g2d
+endif
 
 $(eval $(meson-package))
