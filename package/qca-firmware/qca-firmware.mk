78991352a074b27fb51c177b97156204bbb79a1c
Gary Bisson <gary.bisson@boundarydevices.com>
2021-11-09 17:06:21 +0100

    qca-firmware: add support for pci modules
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- package/qca-firmware/qca-firmware.mk
+++ package/qca-firmware/qca-firmware.mk
@@ -4,7 +4,11 @@
 #
 ################################################################################
 
+ifeq ($(BR2_PACKAGE_QCA_FIRMWARE_QCA6174_PCIE),y)
+QCA_FIRMWARE_VERSION = 8376bcd5e28a954b6cb522b079d3a94ddf22a756
+else
 QCA_FIRMWARE_VERSION = af26e1e5670bb9afa0691d260924adf86c37fea3
+endif
 QCA_FIRMWARE_SITE = \
        $(call github,boundarydevices,qca-firmware,$(QCA_FIRMWARE_VERSION))
 QCA_FIRMWARE_LICENSE = BSD-3-Clause
