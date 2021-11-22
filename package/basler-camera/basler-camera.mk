7e45d7d78fa0417bc00ee87414cbc3cfd2ec8807
Gary Bisson <gary.bisson@boundarydevices.com>
2021-11-22 21:12:10 +0100

    package/basler-camera: bump to version 4.2.2.13.0
    - also switch to a leaner version of the package
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- package/basler-camera/basler-camera.mk
+++ package/basler-camera/basler-camera.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BASLER_CAMERA_VERSION = 4.2.2.6.0
+BASLER_CAMERA_VERSION = 4.2.2.13.0
 BASLER_CAMERA_SITE = $(FREESCALE_IMX_SITE)
 BASLER_CAMERA_SOURCE = basler-camera-$(BASLER_CAMERA_VERSION).bin
 BASLER_CAMERA_DEPENDENCIES = linux
@@ -18,11 +18,10 @@ define BASLER_CAMERA_EXTRACT_CMDS
 endef
 
 define BASLER_CAMERA_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/opt/imx8-isp/bin
-	cp -r $(@D)/opt/imx8-isp/bin/* $(TARGET_DIR)/opt/imx8-isp/bin
-	ln -sf /opt/imx8-isp/bin/DAA3840_30MC_1080P.drv $(TARGET_DIR)/usr/lib64/DAA3840_30MC_1080P.drv
-	ln -sf /opt/imx8-isp/bin/DAA3840_30MC_4K.drv $(TARGET_DIR)/usr/lib64/DAA3840_30MC_4K.drv
-	cp -r $(@D)/usr/lib/* $(TARGET_DIR)/usr/lib/
+	cp -r $(@D)/opt/* $(TARGET_DIR)/usr/share/
+	ln -sf /usr/share/imx8-isp/bin/DAA3840_30MC_1080P.drv $(TARGET_DIR)/usr/lib64/DAA3840_30MC_1080P.drv
+	ln -sf /usr/share/imx8-isp/bin/DAA3840_30MC_4K.drv $(TARGET_DIR)/usr/lib64/DAA3840_30MC_4K.drv
+	cp -r $(@D)/usr/* $(TARGET_DIR)/usr/
 endef
 
 $(eval $(generic-package))
